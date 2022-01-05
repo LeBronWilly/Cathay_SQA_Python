4e09fd89e2e4f87bd9109491499adb5a10cf49e2
Willy Fang <w70024@gmail.com>
2022-01-05 17:02:25 +0800

    Test

--- tests/test_core_Willy.py
+++ tests/test_core_Willy.py
@@ -7,11 +7,10 @@ from decimal import Decimal, ROUND_DOWN
 
 INIT_MONEY=100.0
 
+customer = Customer("Test User","account")
 
 
-
-
-class TestCoreSuites:
+class TestCoreSuites(Customer):
 #################################
 #
 # 假設這位客戶, 名字是 Test User, 帳號為100-1100, 一開始帳戶會先存100元, 要測試下面項目: 
@@ -19,7 +18,26 @@ class TestCoreSuites:
 # 1. 之後存款 1000 元, 確認帳戶總金額為 1100 元
 # 2. 下一步提款 500 元, 確認帳戶總金額為 600 元
 # 3. 假設銀行年利率是10%, 經過一年之後確認帳戶餘額為660元
-# 4.之後提款 700 元, pytest 預期會接到 RuntimeError
+# 4. 之後提款 700 元, pytest 預期會接到 RuntimeError
 #
 #################################
-    pass
+
+
+    # @pytest.fixture(autouse=True)
+    # def test_setup(self, name, account,INIT_MONEY):
+    #     self.name = "Test User"
+    #     self.account = "100-1100"
+    #     self.INIT_MONEY = INIT_MONEY
+
+    # Test Case
+    def test_customer_account(self):
+        # self.name = "Test User"
+        # self.account = "100-1100"
+        # self.INIT_MONEY = INIT_MONEY
+        assert self.balance+1000 == 1100
+
+
+
+
+
+
